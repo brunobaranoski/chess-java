37dbe879a9ec1eca1dcc10194f4a768e3b828c84
Bruno Teles <bnoteles@gmail.com>
2026-02-23 23:00:55 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,34 @@
+package boardgame;
+
+public class Position {
+	
+	 private int row;
+	 private int column;
+	 
+	 public Position(int row, int color) {
+		this.row = row;
+		this.column = color;
+	 }
+
+	 public int getRow() {
+		 return row;
+	 }
+
+	 public void setRow(int row) {
+		 this.row = row;
+	 }
+
+	 public int getColor() {
+		 return column;
+	 }
+
+	 public void setColor(int color) {
+		 this.column = color;
+	 }
+	 
+	 @Override
+	 public String toString() {
+		 return row + ", " + column;
+	 }
+	 
+}
